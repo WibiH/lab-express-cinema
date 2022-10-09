3027ea23dc5e181b5c34173cafdec8dc39b6b881
Wiebke Hagenauer <wiebosaurus@hotmail.com>
2022-10-09 14:10:31 +0200

    done

---
+++ models/movie.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  stars: {
+    type: [String],
+    index: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  showtimes: [String],
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
